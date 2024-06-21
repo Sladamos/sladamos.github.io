4e773df37a2e3cfef6d95ce4c00f997511745502
Sladamos <sladamo@wp.pl>
2024-06-22 01:09:48 +0200

    Leno properly configured routes

--- Leno/src/app/app.component.ts
+++ Leno/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AppMainComponent } from './views/app-main/app-main.component';
 import { AppNavComponent } from './views/app-nav/app-nav.component';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,17 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.fragment.subscribe(fragment => {
+      if (fragment) {
+        const element = document.getElementById(fragment);
+        if (element) {
+          element.scrollIntoView();
+        }
+      }
+    });
+  }
 }
--- Leno/src/app/views/app-main/app-main.component.ts
+++ Leno/src/app/views/app-main/app-main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './app-main.component.html',
   styleUrl: './app-main.component.css'
 })
