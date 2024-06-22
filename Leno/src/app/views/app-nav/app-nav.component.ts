46bfd9834c8dca96fb06893cb967e2b552241a92
Sladamos <sladamo@wp.pl>
2024-06-22 13:31:05 +0200

    Leno properly working responsive window size service

--- Leno/src/app/views/app-nav/app-nav.component.ts
+++ Leno/src/app/views/app-nav/app-nav.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { DesktopMenuComponent } from './desktop-menu/desktop-menu.component';
 import { MobileMenuComponent } from './mobile-menu/mobile-menu.component';
+import { WindowSizeService } from '../../utils/service/window-size.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [DesktopMenuComponent, MobileMenuComponent],
+  imports: [CommonModule, DesktopMenuComponent, MobileMenuComponent],
   templateUrl: './app-nav.component.html',
-  styleUrl: './app-nav.component.css'
+  styleUrl: './app-nav.component.css',
+  providers: [WindowSizeService]
 })
 export class AppNavComponent {
 
+  constructor(private windowSizeService: WindowSizeService)
+  {
+
+  }
+
+  isMobile(): boolean {
+    return this.windowSizeService.mobileQuery.matches;
+
+  }
 }
