f3a584418321f8cb3fb4eb655a292d86042d8128
Sladamos <sladamo@wp.pl>
2024-06-22 19:12:34 +0200

    Leno properly working mobile hero section

--- Leno/src/app/app.routes.ts
+++ Leno/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { DefaultViewComponent } from './views/default-view/default-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: DefaultViewComponent}
+];
---
+++ Leno/src/app/views/default-view/default-view.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-default-view',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './default-view.component.html',
+  styleUrl: './default-view.component.css'
+})
+export class DefaultViewComponent {
+
+}
