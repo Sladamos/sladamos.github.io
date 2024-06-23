0280be7665d4e3cd41e473aeed451017565dd547
Sladamos <sladamo@wp.pl>
2024-06-23 21:46:37 +0200

    Leno screenshots created

--- Leno/src/app/views/default-view/default-view.component.ts
+++ Leno/src/app/views/default-view/default-view.component.ts
@@ -3,11 +3,12 @@ import { RouterModule } from '@angular/router';
 import { TestimonialsComponent } from './testimonials/testimonials.component';
 import { FeaturesComponent } from './features/features.component';
 import { HeroComponent } from './hero/hero.component';
+import { ScreenshotsComponent } from './screenshots/screenshots.component';
 
 @Component({
   selector: 'app-default-view',
   standalone: true,
-  imports: [RouterModule, TestimonialsComponent, FeaturesComponent, HeroComponent],
+  imports: [RouterModule, TestimonialsComponent, FeaturesComponent, HeroComponent, ScreenshotsComponent],
   templateUrl: './default-view.component.html',
   styleUrl: './default-view.component.css'
 })
