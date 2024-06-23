8fbc2af1849c1c724ef8b748f48891acd3876c2f
Sladamos <sladamo@wp.pl>
2024-06-23 21:12:39 +0200

    Leno desktop footer and all necessary components

---
+++ Leno/src/app/views/details-view/details-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './details-view.component.html',
+  styleUrl: './details-view.component.css'
+})
+export class DetailsViewComponent {
+
+}
