b0914a9bf9085e5270e05be05c7cca42b1f9a793
Sladamos <sladamo@wp.pl>
2024-06-24 10:01:20 +0200

    Leno details html finished

--- Leno/src/app/views/default-view/default-view.component.ts
+++ Leno/src/app/views/default-view/default-view.component.ts
@@ -5,11 +5,13 @@ import { FeaturesComponent } from './features/features.component';
 import { HeroComponent } from './hero/hero.component';
 import { ScreenshotsComponent } from './screenshots/screenshots.component';
 import { DownloadComponent } from './download/download.component';
+import { DetailsComponent } from './details/details.component';
 
 @Component({
   selector: 'app-default-view',
   standalone: true,
-  imports: [RouterModule, TestimonialsComponent, FeaturesComponent, HeroComponent, ScreenshotsComponent, DownloadComponent],
+  imports: [RouterModule, TestimonialsComponent, FeaturesComponent, HeroComponent,
+     ScreenshotsComponent, DownloadComponent, DetailsComponent],
   templateUrl: './default-view.component.html',
   styleUrl: './default-view.component.css'
 })
