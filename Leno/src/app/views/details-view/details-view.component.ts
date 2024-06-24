9075eca9459b16469f80982539e9958031bd9777
Sladamos <sladamo@wp.pl>
2024-06-24 22:09:33 +0200

    Leno properly working details features on mobile

--- Leno/src/app/views/details-view/details-view.component.ts
+++ Leno/src/app/views/details-view/details-view.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { InnerHeaderComponent } from '../../utils/view/inner-header/inner-header.component';
+import { DetailsFeaturesComponent } from './features/details-features.component';
 
 @Component({
   selector: 'app-details-view',
   standalone: true,
-  imports: [InnerHeaderComponent],
+  imports: [InnerHeaderComponent, DetailsFeaturesComponent],
   templateUrl: './details-view.component.html',
   styleUrl: './details-view.component.css'
 })
