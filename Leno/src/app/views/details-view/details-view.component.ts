a716ee7095e02c44e1f80e37e929fbded77bf6bc
Sladamos <sladamo@wp.pl>
2024-06-25 00:36:07 +0200

    Leno pricing-notes finished and preparation for pricing

--- Leno/src/app/views/details-view/details-view.component.ts
+++ Leno/src/app/views/details-view/details-view.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { InnerHeaderComponent } from '../../utils/view/inner-header/inner-header.component';
 import { DetailsFeaturesComponent } from './features/details-features.component';
+import { DetailsPricingComponent } from './pricing/details-pricing.component';
 
 @Component({
   selector: 'app-details-view',
   standalone: true,
-  imports: [InnerHeaderComponent, DetailsFeaturesComponent],
+  imports: [InnerHeaderComponent, DetailsFeaturesComponent, DetailsPricingComponent],
   templateUrl: './details-view.component.html',
   styleUrl: './details-view.component.css'
 })
