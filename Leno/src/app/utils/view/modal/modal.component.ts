50641b4aa9c4c2352675c660d4c75387f1fae1bc
Sladamos <sladamo@wp.pl>
2024-06-25 14:22:58 +0200

    Leno url sanitization

--- Leno/src/app/utils/view/modal/modal.component.ts
+++ Leno/src/app/utils/view/modal/modal.component.ts
@@ -1,21 +1,29 @@
-import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
-import { VideoService } from '../../service/video.service';
+import { Component, ElementRef, Renderer2, SecurityContext, ViewChild } from '@angular/core';
+import { VideoMessage, VideoService } from '../../service/video.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { SafePipe } from '../../pipe/safe.pipe';
 
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [],
+  imports: [SafePipe],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.css'
 })
 export class ModalComponent {
   @ViewChild('video') video!: ElementRef;
+  videoUrl: string = "";
 
   constructor(private renderer: Renderer2) {
+
   }
 
   exitFromVideo() {
     const element = this.video.nativeElement;
     this.renderer.setStyle(element, 'display', 'none');
+  }
+
+  enterToVideo(message: VideoMessage) {
+    this.videoUrl = message.url;
   }
 }
