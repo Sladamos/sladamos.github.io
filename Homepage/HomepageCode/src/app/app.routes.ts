149e816b43a6b795a09a3012d870e0cbf22318c6
Sladamos <sladamo@wp.pl>
2025-02-25 22:49:30 +0100

    Search bar

--- Homepage/HomepageCode/src/app/app.routes.ts
+++ Homepage/HomepageCode/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {MainPageComponent} from './core/main/view/main-page/main-page.component';
 import {ExperiencePageComponent} from './features/experience/view/experience-page/experience-page.component';
+import {ProjectsPageComponent} from './features/project/view/projects-page/projects-page.component';
 
 export const routes: Routes = [
   { path: '', component: MainPageComponent },
-  { path: 'experience', component: ExperiencePageComponent}
+  { path: 'experience', component: ExperiencePageComponent},
+  { path: 'projects', component: ProjectsPageComponent}
 ];
