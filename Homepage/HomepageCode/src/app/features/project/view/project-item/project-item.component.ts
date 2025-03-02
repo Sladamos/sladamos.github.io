ed6d68ac6a8d70e8d8689ee77321abd547f2d0aa
Sladamos <sladamo@wp.pl>
2025-03-02 16:36:01 +0100

    Removed reversed approach

--- Homepage/HomepageCode/src/app/features/project/view/project-item/project-item.component.ts
+++ Homepage/HomepageCode/src/app/features/project/view/project-item/project-item.component.ts
@@ -1,5 +1,5 @@
 import {ChangeDetectionStrategy, Component, input, InputSignal} from '@angular/core';
-import { ProjectModel } from '../../model/project-model';
+import {ProjectModel} from '../../model/project-model';
 import {FaIconComponent} from '@fortawesome/angular-fontawesome';
 import {faGithub} from '@fortawesome/free-brands-svg-icons';
 import {ProjectTechnologyChipComponent} from '../project-technology-chip/project-technology-chip.component';
@@ -13,13 +13,9 @@ import {ProjectTechnologyChipComponent} from '../project-technology-chip/project
   templateUrl: './project-item.component.html',
   styleUrl: './project-item.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  host: {
-    '[class.reversed]' : 'isReversed()'
-  }
 })
 export class ProjectItemComponent {
   project: InputSignal<ProjectModel> = input.required<ProjectModel>();
-  isReversed = input(false);
 
   protected readonly faGithub = faGithub;
 }
