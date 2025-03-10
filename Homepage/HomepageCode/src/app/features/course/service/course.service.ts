7cf76f2f44a46f7db674dc679271ccfa3bd674f6
Sladamos <sladamo@wp.pl>
2025-03-10 23:08:12 +0100

    Udemy courses

--- Homepage/HomepageCode/src/app/features/course/service/course.service.ts
+++ Homepage/HomepageCode/src/app/features/course/service/course.service.ts
@@ -8,19 +8,60 @@ import {TechnologyService} from '../../technology/service/technology.service';
 export class CourseService {
 
   private technologyService = inject(TechnologyService);
+  private readonly issuers = {
+    'Udemy': 'issuer_udemy.jpg'
+  };
   courses = signal<CourseModel[]>([
+    /*
     {
-      title: 'Angular - The Complete Guide (2025 Edition) ',
-      issuerIconUrl: 'issuer_udemy.jpg',
+      title: '',
+      issuerIconUrl: this.issuers[''],
+      issuerName: '',
+      technologies: [this.technologyService.getTechnology('')],
+      certificateUrl: 'course_.pdf'
+    },*/
+    {
+      title: 'Angular - The Complete Guide',
+      issuerIconUrl: this.issuers['Udemy'],
       issuerName: 'Udemy',
       technologies: [this.technologyService.getTechnology('Angular')],
       certificateUrl: 'course_angularComplete.pdf'
     },
     {
-      title: '',
-      issuerIconUrl: 'issuer_.jpg',
-      issuerName: '',
-      technologies: [this.technologyService.getTechnology('')],
-      certificateUrl: 'course_.pdf'
+      title: 'Modern HTML & CSS From The Beginning 2.0',
+      issuerIconUrl: this.issuers['Udemy'],
+      issuerName: 'Udemy',
+      technologies: [this.technologyService.getTechnology('CSS')],
+      certificateUrl: 'course_modernHtmlCss.pdf'
     },
+    {
+      title: 'Design Patterns in Java',
+      issuerIconUrl: this.issuers['Udemy'],
+      issuerName: 'Udemy',
+      technologies: [this.technologyService.getTechnology('Java')],
+      certificateUrl: 'course_designPatternsJava.pdf'
+    },
+    {
+      title: 'Practical Test Driven Development for Java Programmers',
+      issuerIconUrl: this.issuers['Udemy'],
+      issuerName: 'Udemy',
+      technologies: [this.technologyService.getTechnology('Java'), this.technologyService.getTechnology('Mockito')],
+      certificateUrl: 'course_practicalTDDJava.pdf'
+    },
+    {
+      title: 'Master Object Oriented Design in Java',
+      issuerIconUrl: this.issuers['Udemy'],
+      issuerName: 'Udemy',
+      technologies: [this.technologyService.getTechnology('Java')],
+      certificateUrl: 'course_masterOOOJava.pdf'
+    },
+    {
+      title: 'Docker & Kubernetes: The Practical Guide',
+      issuerIconUrl: this.issuers['Udemy'],
+      issuerName: 'Udemy',
+      technologies: [this.technologyService.getTechnology('Docker'), this.technologyService.getTechnology('Kubernetes')],
+      certificateUrl: 'course_dockerKubernetesComplete.pdf'
+    }
+  ])
 }
+
