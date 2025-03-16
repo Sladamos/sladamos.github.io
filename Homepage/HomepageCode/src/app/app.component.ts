e818d53f2fd2aff23982447f4a353a0e58c06f4c
Sladamos <sladamo@wp.pl>
2025-03-16 22:46:31 +0100

    Back to top button

--- Homepage/HomepageCode/src/app/app.component.ts
+++ Homepage/HomepageCode/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {FooterComponent} from './core/shared/view/footer/footer.component';
 import {HeaderComponent} from './core/shared/view/header/header.component';
+import {ReturnTopButtonComponent} from './core/shared/view/return-top-button/return-top-button.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent, HeaderComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, ReturnTopButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
