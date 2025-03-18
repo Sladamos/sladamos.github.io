c56e609cf4ca8db84b698eab698644b438a93db2
Sladamos <sladamo@wp.pl>
2025-03-18 22:15:05 +0100

    Using properly host directive

--- Homepage/HomepageCode/src/app/features/experience/view/experience-item/experience-item.component.ts
+++ Homepage/HomepageCode/src/app/features/experience/view/experience-item/experience-item.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, HostBinding, inject, input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, input} from '@angular/core';
 import {ExperienceSectionItem} from '../../model/experience-section-item';
 import {ScreenTypeService} from '../../../../core/shared/service/screen-type.service';
 
@@ -7,14 +7,13 @@ import {ScreenTypeService} from '../../../../core/shared/service/screen-type.ser
   imports: [],
   templateUrl: './experience-item.component.html',
   styleUrl: './experience-item.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '[class.container__normal]': 'screenTypeService.isNotDesktop()',
+    '[class.container__tiny]': '!screenTypeService.isNotDesktop()'
+  }
 })
 export class ExperienceItemComponent {
   screenTypeService = inject(ScreenTypeService);
   item = input.required<ExperienceSectionItem>();
-
-  @HostBinding('class')
-  get containerClass() {
-    return this.screenTypeService.isNotDesktop() ? 'container__normal' : 'container__tiny';
-  }
 }
