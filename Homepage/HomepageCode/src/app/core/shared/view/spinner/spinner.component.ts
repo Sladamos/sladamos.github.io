8c1f23b4970475f23aa57ff568c5a256d1431956
Sladamos <sladamo@wp.pl>
2025-12-16 22:26:28 +0100

    Main page optimized

--- Homepage/HomepageCode/src/app/core/shared/view/spinner/spinner.component.ts
+++ Homepage/HomepageCode/src/app/core/shared/view/spinner/spinner.component.ts
@@ -1,12 +1,4 @@
-import {
-  AfterViewInit,
-  ChangeDetectionStrategy,
-  Component,
-  ContentChildren,
-  ElementRef,
-  QueryList,
-  ViewChild
-} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 
 @Component({
   selector: 'app-spinner',
@@ -15,16 +7,5 @@ import {
   styleUrl: './spinner.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SpinnerComponent implements AfterViewInit {
-  @ContentChildren('spinnerChild', { descendants: true }) children!: QueryList<ElementRef>;
-  @ViewChild('spinnerFront', { static: true }) spinnerFront!: ElementRef<HTMLDivElement>;
-  @ViewChild('spinnerBack', { static: true }) spinnerBack!: ElementRef<HTMLDivElement>;
-
-  ngAfterViewInit() {
-    const firstChild = this.children.first.nativeElement;
-    const secondChild = this.children.toArray()[1]?.nativeElement;
-
-    this.spinnerFront.nativeElement.appendChild(firstChild);
-    this.spinnerBack.nativeElement.appendChild(secondChild);
-  }
+export class SpinnerComponent {
 }
